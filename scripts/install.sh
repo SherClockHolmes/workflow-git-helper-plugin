a7bf2786ab6ec5fa87ceeb179617a728dc6b6a4f
Ethan Holmes <sherclockholmes@gmail.com>
2019-04-21 15:16:17 -0700

    Fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-curl -sSL "https://github.com/workflow-ci/workflow-git-helper-plugin/releases/download/${WORKFLOW_PLUGIN_VERSION}/${WORKFLOW_PLUGIN_VERSION}.tar.gz" -o "git-helper_${WORKFLOW_PLUGIN_VERSION}.tar.gz"
+curl -sSL "https://github.com/workflow-ci/workflow-git-helper-plugin/releases/download/${WORKFLOW_PLUGIN_VERSION}/git-helper_${WORKFLOW_PLUGIN_VERSION}.tar.gz" -o "git-helper_${WORKFLOW_PLUGIN_VERSION}.tar.gz"
 tar xzf "git-helper_${WORKFLOW_PLUGIN_VERSION}.tar.gz"
 mv git-helper /usr/local/bin
 rm "git-helper_${WORKFLOW_PLUGIN_VERSION}.tar.gz"
